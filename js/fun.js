5e059f776bdda3b64cc9c8ceaa91ce19439c5bbc
FuckMeee <471567032@qq.com>
2015-08-25 18:08:45 +0800

    825

--- js/fun.js
+++ js/fun.js
@@ -26,11 +26,63 @@ $(document).ready(function() {
 
  	$('.span1').bind("click", function(){
  		var checkboxId = $(this).attr('name');
-    	$('.span1').toggleClass('check');
-    	if($(".span1").hasClass('check')){
+    $('.span1').toggleClass('check');
+
+
+    if($(".span1").hasClass('check')){
     		$('#' + checkboxId).attr('checked', 'checked');
+        $("#submit").attr("disabled", false);
 		}else {
 			$('#' + checkboxId).removeAttr('checked')
+      $("#submit").attr("disabled", true);
 		}
  	});
+
+  $('.tgy1').bind("click", function(){
+    var checkboxId = $(this).attr('name');
+      $('.tgy1').toggleClass('check');
+      if($(".tgy1").hasClass('check')){
+        $('#' + checkboxId).attr('checked', 'checked');
+        $(".tgyOne").addClass("tgyShow");
+    }else {
+      $('#' + checkboxId).removeAttr('checked')
+      $(".tgyOne").removeClass("tgyShow");
+    }
+  });
+
+
+  $('.tgy2').bind("click", function(){
+    var checkboxId = $(this).attr('name');
+      $('.tgy2').toggleClass('check');
+      if($(".tgy2").hasClass('check')){
+        $('#' + checkboxId).attr('checked', 'checked');
+        $(".tgyTwo").addClass("tgyShow");
+    }else {
+      $('#' + checkboxId).removeAttr('checked')
+      $(".tgyTwo").removeClass("tgyShow");
+    }
+  });
+
+
+  $("#phone").focus(function(){
+    if($(this).val()=="请填写真实号码，用于登录验证和密码找回"){
+      $(this).val("");
+    }
+  }).blur(function(){
+    if($(this).val()==""){
+      $(this).val("请填写真实号码，用于登录验证和密码找回");
+    }
+  });
+
+  $("#qq").focus(function(){
+    if($(this).val()=="请填写真实QQ号，没有QQ号可填写123456"){
+      $(this).val("");
+    }
+  }).blur(function(){
+    if($(this).val()==""){
+      $(this).val("请填写真实QQ号，没有QQ号可填写123456");
+    }
+  });
+
+
 });
